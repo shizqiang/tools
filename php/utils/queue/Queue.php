3a3c2267af20abbfd2618f1c0ed1782ed6aea354
shizq <shizhiqiang@acctrue.com>
2020-09-15 15:11:28 +0800

    add queue

---
+++ php/utils/queue/Queue.php
@@ -0,0 +1,32 @@
+<?php
+namespace utils\queue;
+
+use utils\db\Storage;
+
+abstract class Queue {
+    
+    const Q = 'QUEUE:';
+    
+    public $data;
+    
+    static function dispach($message, $queue = 'default') {
+        $task = new static();
+        $task->data = $message;
+        $redis = Storage::redis();
+        $redis->lPush(static::Q . $queue, serialize($task));
+    }
+    
+    static function handle($queue = 'default') {
+        $redis = Storage::redis();
+        $str = $redis->rPop(static::Q . $queue);
+        if (!$str) {
+            return false;
+        }
+        $task = unserialize($str);
+        $task->run();
+        return true;
+    }
+    
+    abstract function run();
+
+}
