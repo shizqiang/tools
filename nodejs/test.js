c2e4ad52db458cfffa7eb478d29db5fdf3c4e321
shizq <shizqiang@qq.com>
2020-09-16 15:12:12 +0800

    nodejs

--- nodejs/test.js
+++ nodejs/test.js
@@ -3,6 +3,15 @@ let user = '051011477981'
 let pass = '32090713'
 //console.log(module.paths);
 	
+var axios = require('axios');
+
+async function show() {
+	var response = await axios.get('https://dev.lsa0.cn/upload.php');
+	console.log(response.data);
+}
+
+show();
+
 var express = require('express');
 var fs = require('fs');
 
@@ -10,11 +19,9 @@ var fs = require('fs');
 var ini = require('ini');
 var config = ini.parse(fs.readFileSync('.env', 'utf-8'));
 
-
 process.env.NODE_ENV = config.app.env;
 process.env.PORT = config.app.port;
 
 // npm i express --registry=https://registry.npm.taobao.org
 var app = express();
 global.config = config
-console.log(app)
