09f237b662085bcdaa39992276b5db970fe63f2f
shizq <shizhiqiang@acctrue.com>
2020-09-30 17:31:07 +0800

    fix

--- nodejs/test.js
+++ nodejs/test.js
@@ -1,16 +1,21 @@
-//console.log(__dirname)
+console.log(__dirname)
 let user = '051011477981'
 let pass = '32090713'
-//console.log(module.paths);
+console.log(module.paths);
 	
 var axios = require('axios');
 
 async function show() {
-	var response = await axios.get('https://dev.lsa0.cn/upload.php');
-	console.log(response.data);
+	try {
+		var response = await axios.get('https://dev.lsa0.cn/upload.php');
+		console.log(response.data);
+	} catch(e) {
+		console.log(e.message)
+	}
 }
 
-show();
+show(); // 这里调用后会立即结束，并顺序执行下一行代码，所以在这里使用try是捕获不到show方法里的异常的
+console.log('show exec pass');
 
 var express = require('express');
 var fs = require('fs');
